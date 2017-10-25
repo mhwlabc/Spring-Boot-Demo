46bb2afcfd209acad21531fce6fe180b2750821c
zhaolei <2290938350@qq.com>
2017-10-25 15:44:43 +0800

    update

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello World"));
 	}
 
 }
